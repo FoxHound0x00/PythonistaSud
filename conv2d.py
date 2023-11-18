55bda56a02492dac2a5aaf56f2d3ca4178771c37
FoxHound <60322825+FoxHound0x00@users.noreply.github.com>
2023-11-18 19:46:02 +0530

    Update conv2d.py

--- conv2d.py
+++ conv2d.py
@@ -1,17 +1,22 @@
 import numpy as np
 
-def conv(_img,kernel_):
-    iw , ih , ch_ = _img.shape
-    kw , kh = kernel_.shape
-    fw , fh = iw - kw + 1 , ih - kh + 1
-    op_ = np.zeros((fw,fh))
-    
-    for i in range(fw):
-        for j in range(fh):
-            for k in range(ch_):
-                patch = _img[i:i+kw,j:j+kh,k]
-                op_[i,j] += np.dot(patch.flatten(),kernel_.flatten())
-    return op_
-img_ = np.random.random((224,224,3))
-kernel_ = np.random.random((3,3))
-x = conv(img_,kernel_)
+
+def conv(ip, kernel, padding=None, dilation=None, stride=None):
+    iw, ih = ip.shape
+    kw, kh = kernel.shape
+    ow, oh = iw - kw + 1, ih - kh + 1
+    op = np.zeros((ow, oh))
+
+    if padding >= kh:
+        return "Padding size should be smaller than the kernel size"
+
+    for i in range(ow):
+        for j in range(oh):
+            patch = ip[i : i + kw, j : j + kh]
+            op[i, j] += np.dot(patch.flatten(), kernel.flatten())
+    return op
+
+
+arr = np.random.rand(5, 5)
+kernel = np.random.rand(2, 2)
+print(conv(arr, kernel, padding=3))
